4b011689855fe974f95b090800bca57e6261d1b2
Scott Hamilton <shamilton1@gmail.com>
2024-12-02 12:42:24 +1100

    small changes for setting up .env files from root and frontend

--- start.sh
+++ start.sh
@@ -5,15 +5,29 @@ if [ "$1" == "build" ]
 then
   echo "rebuilding..."
   cd frontend
+  if [ ! -f ".env" ]
+  then
+    echo -e "Edit the .env file in the frontend folder before building"
+    echo -e 'VITE_AI_API=http://localhost:3001\nVITE_TTS_KEY=\nVITE_TTS_REGION=australiaeast' >.env
+    exit -1
+  fi
   npm install
   npm run build
   cd ../
   docker compose build
 fi
 
-#export OPENAI_API_KEY="your API key"  # only set either OPENAI_API_KEY or GOOGLE_API_KEY not both.
-#export GOOGLE_API_KEY="your API key"  # only set either OPENAI_API_KEY or GOOGLE_API_KEY not both.
+# use a .env file to store your API key, this must not be added to Git, and keeps this file clean.
+if [ ! -f ./.env ]
+then
+  echo -e '#GOOGLE_API_KEY=""\n#OPENAI_API_KEY=""' >.env
+  echo -e "Stopped\n Edit .env with either your Google AI Studio or OpenAI API key"
+  exit -1
+else
+  . ./.env
+fi
+
+env | grep -i "API_KEY"
 
 echo "Starting..."
 docker compose up
-
